388cd91ace4d8cd95072e570c2e2d659537effc0
蔡国伟 <785244892@qq.com>
2020-04-15 20:39:34 +0800

    第一次提交

---
+++ src/main/java/com/mcl/tools/bailing/JsonUtils.java
@@ -0,0 +1,75 @@
+package com.mcl.tools.bailing;
+
+import com.google.gson.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @auth caiguowei
+ * @date 2020/4/14
+ */
+public class JsonUtils {
+
+    public static String toJsonString(Object obj){
+        return new Gson().toJson(obj);
+    }
+
+    public static <T> T parse(String jsonStr, Class<T> clazz){
+        return new Gson().fromJson(jsonStr, clazz);
+    }
+    public static <T> List<T> parseList(String jsonStr, Class<T> cls) {
+        Gson gson = new Gson();
+        List<T> list = new ArrayList<T>();
+        JsonArray array = new JsonParser().parse(jsonStr).getAsJsonArray();
+        for (final JsonElement elem : array) {
+            list.add(gson.fromJson(elem, cls));
+        }
+        return list;
+    }
+
+
+
+    public static Builder builder(){
+        return new Builder();
+    }
+
+    public static class Builder{
+        JsonObject jsonObject = new JsonObject();
+        public Builder add(String key, String val){
+            jsonObject.addProperty(key,val);
+            return this;
+        }
+        public Builder add(String key, Number val){
+            jsonObject.addProperty(key,val);
+            return this;
+        }
+        public Builder add(String key, Boolean val){
+            jsonObject.addProperty(key,val);
+            return this;
+        }
+        public Builder add(String key, Character val){
+            jsonObject.addProperty(key,val);
+            return this;
+        }
+
+        public Builder remove(String key){
+            jsonObject.remove(key);
+            return  this;
+        }
+
+        public JsonObject build(){
+            return jsonObject;
+        }
+
+        public String toString(){
+            return jsonObject.toString();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        JsonObject build = JsonUtils.builder().add("a", "b").add("b", 123).add("c", true).remove("c").build();
+        System.out.println(build);
+    }
+}
